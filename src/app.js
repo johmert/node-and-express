6f23fb88640d14866e1c85338e7e9769e82c43b0
johmert <johmert@users.noreply.github.com>
2022-02-04 00:15:51 -0800

    middleware functions

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const app = express();
 
+// middleware function
+const sayHello = (req, res, next) => {
+    //when called, respond with "Hello!"
+    res.send("Hello!");
+};
+
+// middleware function
+const logging = (req, res, next) => {
+    // log it
+    console.log("A request is being made!");
+    // after function body, move on to next middleware function
+    next();
+};
+
+app.use(logging);
+app.use(sayHello);
+
+
 module.exports = app;
