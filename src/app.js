e1f81969399c3d78c036087825c552baf39c8ecf
johmert <johmert@users.noreply.github.com>
2022-02-04 12:43:06 -0800

    query and route parameter hello world

--- src/app.js
+++ src/app.js
@@ -3,8 +3,20 @@ const app = express();
 
 // middleware function
 const sayHello = (req, res, next) => {
-    //when called, respond with "Hello!"
-    res.send("Hello!");
+    //log the request query object
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}` : "Hello!";
+    //when 'sayHello' is called, respond with content
+    res.send(content);
+};
+
+const saySomething = (req, res) => {
+    //params set in url like :greeting
+    const greeting = req.params.greeting;
+    const name = req.query.name;
+    const content = greeting && name ?  `${greeting}, ${name}!` : `${greeting}!`;
+    res.send(content);
 };
 
 const morgan = require("morgan");
@@ -12,6 +24,9 @@ const morgan = require("morgan");
 app.use(morgan("dev"));
 //creates a path to "/hello" that runs the sayHello middleware function
 app.get("/hello", sayHello);
+//creates a path to "/say/:greeting" that runs saySomething
+// go to localhost:8080/say/hello?name=world to see "hello, world!"
+app.get("/say/:greeting", saySomething);
 
 
 module.exports = app;
